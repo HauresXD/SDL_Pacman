aa9b07c5ce9ee4ea76cc59d09af0af164e86b04d
HauresXD <pmarkvart@protonmail.com>
2023-11-22 10:25:27 +0100

    basic setup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SDL_Pacman
+
+Pacman game made in C with SDL2.
+
+compile by:
+```bash
+gcc main.c -o main -lSDL2
+```
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+int main(void) {
+
+    return 0;
+}
